5d2f803e6ad1fd357f6041da50176068a0724a4c
chandanbhat <chandanbhat9799@gmail.com>
2020-06-21 06:55:54 +0000

    dash updated

--- dash/views.py
+++ dash/views.py
@@ -11,5 +11,18 @@ def home(request):
     row1 = [0,1,2,3]
     row3 = [0,1,2]
 
+    current_user = request.user
+    if(current_user.is_superuser):
+        is_admin = "Yes"
+    else:
+        is_admin = "No"
+
+    row1 = [
+        {'header':'Current User','body':current_user.username,'icon_class':'fa fa-user'},
+        {'header':'Last Login','body':current_user.last_login,'icon_class':'fa fa-clock-o'},
+        {'header':'Date Joined','body':current_user.date_joined,'icon_class':'fa fa-calendar'},
+        {'header':'Admin Access','body':is_admin,'icon_class':'fa fa-unlock-alt'}
+        ]
+
     context = {'row1':row1,'row3':row3}
     return render(request, 'dash/home.html', context=context)
