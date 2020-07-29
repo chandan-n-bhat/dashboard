ef697de139db6a4f5361651cb822bb4cb58eab87
chandanbhat <chandanbhat9799@gmail.com>
2020-07-29 18:09:46 +0530

    labels for doughnutchart removed

--- dash/static/dash/js/home.js
+++ dash/static/dash/js/home.js
@@ -29,17 +29,17 @@ function IS(bar_labels,bar_values){
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    // 'rgba(75, 192, 192, 0.2)',
-                    // 'rgba(153, 102, 255, 0.2)',
-                    // 'rgba(255, 159, 64, 0.2)'
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
-                    // 'rgba(75, 192, 192, 1)',
-                    // 'rgba(153, 102, 255, 1)',
-                    // 'rgba(255, 159, 64, 1)'
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 2
             }]
@@ -125,7 +125,7 @@ function DPC(dpc_labels,dpc_values,pc_labels,pc_values,dpc2_labels,dpc2_values){
         datasets: [
             {
                 // label: "Population (millions)",
-                backgroundColor: ["#ff3d66","#36a2eb"],
+                backgroundColor: ["#ff3d66","#36a2eb","#FF681F","#FDFF00"],
                 // data: [5267,433]
                 data: dpc_values
             }
@@ -152,6 +152,9 @@ function DPC(dpc_labels,dpc_values,pc_labels,pc_values,dpc2_labels,dpc2_values){
         options: {
             responsive:true,
             maintainAspectRatio: false,
+            legend: {
+                display: false
+            }
         }
     });
 
@@ -162,7 +165,7 @@ function DPC(dpc_labels,dpc_values,pc_labels,pc_values,dpc2_labels,dpc2_values){
         // labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
         labels: pc_labels,
         datasets: [{
-            backgroundColor: ["#00203FFF", "#ADEFD1FF"],
+            backgroundColor: ["#00203FFF","#F39C12","#05ffa6"],
             // data: [2478,5267,734,784,433]
             data: pc_values
         }]
