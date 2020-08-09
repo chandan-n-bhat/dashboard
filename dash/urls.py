7dd74e4bc0b7a9dc26304c4972a8a9ce0ccc9413
chandanbhat <chandanbhat9799@gmail.com>
2020-08-09 23:36:42 +0530

    urls renamed

--- dash/urls.py
+++ dash/urls.py
@@ -5,7 +5,7 @@ app_name = 'dash'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('api/v1/dpc', views.dpc, name='doughnutPieChart'),
-    path('api/v1/yearWiseSales', views.yearWiseSales, name='yearWiseSale'),
-    path('api/v1/itemsales', views.itemSales, name='itemSales')
+    path('api/v1/doughnutChart', views.doughnutChart, name='doughnutChart'),
+    path('api/v1/lineChart', views.lineChart, name='lineChart'),
+    path('api/v1/barGraph', views.barGraph, name='barGraph')
 ]
